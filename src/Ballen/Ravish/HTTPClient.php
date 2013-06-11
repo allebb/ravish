8af9dd692466eeb3f5a5001717ea6fe64e51afea
Bobby Allen <bobbyallen.uk@gmail.com>
2013-06-11 12:10:13 +0100

    Adding initial code and composer.json file.

---
+++ src/Ballen/Ravish/HTTPClient.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Ravish
+ *
+ * Ravish is a composer/PSR-0 compatible basic HTTP Client for PHP.
+ *
+ * @author bobbyallen.uk@gmail.com (Bobby Allen)
+ * @version 1.0.0
+ * @license http://opensource.org/licenses/MIT
+ * @link https://github.com/bobsta63/ravish
+ * @link http://www.bobbyallen.me
+ *
+ */
+
+namespace Ballen\Ravish;
+
+class HTTPClient
+{
+
+}
+
+?>
